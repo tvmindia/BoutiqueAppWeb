68e2dff35628a141fdfec9c0e54fd07e2e820d02
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-04-20 16:20:29 +0530

    product webservice and curresponding DAL

--- Boutique/DAL/Product.cs
+++ Boutique/DAL/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -76,7 +78,114 @@ namespace Boutique.DAL
         #endregion properties
 
         #region Methods
-       
+          
+            #region Constructors
+            public Product()
+            {
+            }
+            public Product(string ProductID,string BoutiqueID)
+            {
+                this.ProductID = ProductID;
+                this.BoutiqueID = BoutiqueID;
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlDataAdapter sda = null;
+                DataTable dt = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[SelectProductByProductID]";
+                    cmd.Parameters.Add("@ProductID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.ProductID);
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);                    
+                    sda.SelectCommand = cmd;
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    if (dt.Rows.Count == 0) { throw new Exception("No such ID"); }
+                    DataRow row = dt.NewRow();
+                    row = dt.Rows[0];
+                    this.ProductID = row["ProductID"].ToString();
+                    Name = row["Name"].ToString();
+                    Description = row["Description"].ToString();
+                    Price = long.Parse(row["Price"].ToString());
+                    IsOutOfStock = Boolean.Parse(row["IsOutOfStock"].ToString());
+                    IsActive = Boolean.Parse(row["IsActive"].ToString());
+                    Categories = row["Categories"].ToString();
+                    DesignerID = row["DesignerID"].ToString();
+                    CreatedBy = row["CreatedBy"].ToString();
+                    CreatedDate = DateTime.Parse(row["CreatedDate"].ToString());
+                    UpdatedBy = row["UpdatedBy"].ToString();
+                    UpdatedDate = DateTime.Parse(row["UpdatedDate"].ToString());
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+            }
+            #endregion Constructors
+
+            #region Product details as DataTable
+        /// <summary>
+        /// To get a product's details by ProductID
+        /// </summary>
+        /// <returns>Datatable of details</returns>
+            public DataTable GetProductByProductID()
+            {
+                if (ProductID == "")
+                {
+                    throw new Exception("ProductID is Empty!!");
+                }
+                if (BoutiqueID == "")
+                {
+                    throw new Exception("BoutiqueID is Empty!!");
+                }
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlDataAdapter sda = null;
+                DataTable dt = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    sda = new SqlDataAdapter();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[SelectProductByProductID]";
+                    cmd.Parameters.Add("@ProductID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.ProductID);
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);  
+                    sda.SelectCommand = cmd;
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    if (dt.Rows.Count == 0) { throw new Exception("No such item"); }
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+            }
+            #endregion
+
+
         #endregion Methods
     }
 }
--- Boutique/WebServices/WebService.asmx.cs
+++ Boutique/WebServices/WebService.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using Boutique.DAL;
+using System.Data;
 
 namespace Boutique.WebServices
 {
@@ -16,11 +18,158 @@ namespace Boutique.WebServices
     [System.Web.Script.Services.ScriptService]
     public class WebService : System.Web.Services.WebService
     {
-
+        #region Products
+        /// <summary>
+        /// To get a product's details by product id
+        /// </summary>
+        /// <param name="productID">Product's ID</param>
+        /// <param name="boutiqueID">Boutique's ID</param>
+        /// <returns>JSON of product details</returns>
         [WebMethod]
-        public string HelloWorld()
+        public string Products(string productID,string boutiqueID)
         {
-            return "Hello World";
+            DataTable dt = new DataTable();
+            try
+            {
+                Product product = new Product();
+                product.ProductID = productID;
+                product.BoutiqueID = boutiqueID;
+                dt=product.GetProductByProductID();
+            }
+            catch (Exception ex)
+            {
+                //Return error message
+                dt = new DataTable();
+                dt.Columns.Add("Flag", typeof(Boolean));
+                dt.Columns.Add("Message", typeof(String));
+                DataRow dr = dt.NewRow();
+                dr["Flag"] = false;
+                dr["Message"] = ex.Message;
+                dt.Rows.Add(dr);
+            }
+            finally
+            {
+            }
+            return getDbDataAsJSON(dt);
         }
+        #endregion Products
+
+        #region JSON converter
+        /// <summary>
+        /// JSON function without returning any images
+        /// </summary>
+        /// <param name="dt">Datatable to be converted</param>
+        /// <returns>dt in JSON format</returns>
+        public String getDbDataAsJSON(DataTable dt)
+        {
+            try
+            {
+                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+                Dictionary<string, object> row;
+                foreach (DataRow dr in dt.Rows)
+                {
+                    row = new Dictionary<string, object>();
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        row.Add(col.ColumnName, dr[col]);
+                    }
+                    rows.Add(row);
+                }
+                this.Context.Response.ContentType = "";
+                return serializer.Serialize(rows);
+            }
+            catch (Exception ex)
+            {
+                //Return error message
+                DataTable ErrorMsg = new DataTable();
+                ErrorMsg.Columns.Add("Flag", typeof(Boolean));
+                ErrorMsg.Columns.Add("Message", typeof(String));
+                DataRow dr = ErrorMsg.NewRow();
+                dr["Flag"] = false;
+                dr["Message"] = ex.Message;
+                ErrorMsg.Rows.Add(dr);
+                return getDbDataAsJSON(ErrorMsg); ;
+            }
+            finally
+            {
+
+            }
+
+        }
+        /// <summary>
+        /// JSON function with returning any images
+        /// </summary>
+        /// <param name="ds">Dataset</param>
+        /// <param name="imgColName">Coloumn names array that contains images(data)</param>
+        /// <param name="imgFileNameCol">Coloumn names array that contain file name</param>
+        /// <param name="imgFileTypeCol">Coloumn names array that contain file type</param>
+        /// <param name="isThumb">Optional parameter to say whether the thumbnail is enough for calling function</param>
+        /// <returns>ds in JSON format with links to images that are temporarly stored in server folder</returns>
+        //public String getDbDataAsJSON(DataSet ds, ArrayList imgColName, ArrayList imgFileNameCol, ArrayList imgFileTypeCol, Boolean isThumb = false)
+        //{
+        //    try
+        //    {
+        //        DataTable dt = ds.Tables[0];
+        //        String filePath = Server.MapPath("~/tempImages/");
+
+        //        System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+        //        List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+        //        Dictionary<string, object> row;
+        //        foreach (DataRow dr in dt.Rows)
+        //        {
+        //            row = new Dictionary<string, object>();
+        //            //adding data in JSON
+        //            foreach (DataColumn col in dt.Columns)
+        //            {
+        //                if (!imgColName.Contains(col.ColumnName))
+        //                {
+        //                    row.Add(col.ColumnName, dr[col]);
+        //                }
+        //            }
+        //            //adding image details in JSON
+        //            for (int i = 0; i < imgColName.Count; i++)
+        //            {
+
+        //                if (dr[imgColName[i] as string] != DBNull.Value)
+        //                {
+        //                    String fileURL = filePath + DateTime.Now.ToString("ddHHmmssfff") + dr[imgFileNameCol[i] as string].ToString().Replace(" ", "_");
+        //                    if (!System.IO.File.Exists(fileURL))
+        //                    {
+        //                        byte[] buffer= (byte[])dr[imgColName[i] as string];
+        //                            System.IO.File.WriteAllBytes(fileURL, buffer);
+        //                    }
+        //                    row.Add(imgColName[i] as string, fileURL);
+        //                }
+
+        //            }
+        //            rows.Add(row);
+        //        }
+
+        //        this.Context.Response.ContentType = "";
+
+        //        return serializer.Serialize(rows);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //Return error message
+        //        DataSet dsError = new DataSet();
+        //        DataTable ErrorMsg = new DataTable();
+        //        ErrorMsg.Columns.Add("Flag", typeof(Boolean));
+        //        ErrorMsg.Columns.Add("Message", typeof(String));
+        //        DataRow dr = ErrorMsg.NewRow();
+        //        dr["Flag"] = false;
+        //        dr["Message"] = ex.Message;
+        //        ErrorMsg.Rows.Add(dr);
+        //        dsError.Tables.Add(ErrorMsg);
+        //        return getDbDataAsJSON(dsError);
+        //    }
+        //    finally
+        //    {
+
+        //    }
+        //}
+        #endregion JSON converter   
     }
 }
