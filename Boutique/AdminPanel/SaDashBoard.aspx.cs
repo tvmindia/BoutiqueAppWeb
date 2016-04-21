dfbf73c3aa35b93e07fa8e6dfc0d8778e5617734
tvm-albert <albert.thomson@thrithvam.ae>
2016-04-21 18:30:43 +0530

    new ajax code for table bind

--- Boutique/AdminPanel/SaDashBoard.aspx.cs
+++ Boutique/AdminPanel/SaDashBoard.aspx.cs
@@ -1,24 +1,76 @@
 ﻿using Boutique.DAL;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Boutique.AdminPanel
 {
+
     public partial class SaDashBoard : System.Web.UI.Page
     {
-        Boutiques boutiqueObj = new Boutiques();
+      private Boutiques boutiqueObj = new Boutiques();
         Users userObj = new Users();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+        #region webmethods
+        [System.Web.Services.WebMethod]
+        public static string GetAllBoutiques()
+        {
+            DataSet ds=null;
+            Boutiques boutiqWebObj = new Boutiques();
+            ds=boutiqWebObj.GetAllBoutiques();
+            
+           //Converting to Json
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Dictionary<string, object> childRow;
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    childRow = new Dictionary<string, object>();
+                    foreach (DataColumn col in ds.Tables[0].Columns)
+                    {
+                        childRow.Add(col.ColumnName, row[col]);
+                    }
+                    parentRow.Add(childRow);
+                }
+                //childRow = new Dictionary<string, object>();
+                //childRow.Add("Result", "Success");
+             
+                //parentRow.Add(childRow);
+            }
+            else
+            {
+                //childRow = new Dictionary<string, object>();
+                //childRow.Add("Result", "Error");
+                //parentRow.Add(childRow);
+            }
+            
+           return jsSerializer.Serialize(parentRow);
+           
+
+          //Converting to Json
+       }
+                    
+     
+        #endregion webmethods
+        #region Methods
+       public void show()
+{
+
+}
+        #endregion Methods
 
 
-
+        #region events
         protected void NewBoutique_ServerClick(object sender, EventArgs e)
         {
             try
@@ -75,5 +127,6 @@ namespace Boutique.AdminPanel
 
             }
         }
+        #endregion events
     }
 }
