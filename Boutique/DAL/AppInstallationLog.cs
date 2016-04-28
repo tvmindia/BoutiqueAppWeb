790270608e83ff3b24a5ccae75b3d67f96ab658f
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-04-28 18:21:41 +0530

    aPP installation log

---
+++ Boutique/DAL/AppInstallationLog.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Boutique.DAL
+{
+    public class AppInstallationLog
+    {   
+        #region Properties
+        public string AppID
+        {
+            get;
+            set;
+        }
+        public string BoutiqueID
+        {
+            get;
+            set;
+        }
+        public string Version
+        {
+            get;
+            set;
+        }
+          public string CreatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime CreatedDate
+        {
+            get;
+            set;
+        }
+        public string UpdatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime UpdatedDate
+        {
+            get;
+            set;
+        }
+	    #endregion
+        
+        
+        #region Methods
+
+        #region AllDesigners
+        public DataTable GetAllAppInstallation()
+        {
+            if (BoutiqueID == "")
+            {
+                throw new Exception("BoutiqueID is Empty!!");
+            }
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            SqlDataAdapter sda = null;
+            DataTable dt = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                sda = new SqlDataAdapter();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[SelectAllAppInstallationsUnderBoutique]";
+                cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);
+                sda.SelectCommand = cmd;
+                dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count == 0) { throw new Exception("No such item"); }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+
+        }
+
+        #endregion AllDesigners
+
+         #endregion Methods
+
+    }
+}
