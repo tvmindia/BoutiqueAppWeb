1f1ae2126adbde423454290a61c709e18533ff73
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-04-28 13:53:13 +0530

    designer dal file

---
+++ Boutique/DAL/Designers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Boutique.DAL
+{
+    public class Designers
+    {
+        #region Properties
+
+        #endregion Properties
+    }
+}
