8f0e520e45d730bd77e3b4fe2e416011b6bdf946
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-04-30 13:24:01 +0530

    DAL for Owners

--- Boutique/DAL/Owners.cs
+++ Boutique/DAL/Owners.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,361 @@ namespace Boutique.DAL
 {
     public class Owners
     {
+        #region Properties
+        public string OwnerID
+        {
+            get;
+            set;
+        }
+        public string BoutiqueID
+        {
+            get;
+            set;
+        }
+        public string UserID
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Phone
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public DateTime DOB
+        {
+            get;
+            set;
+        }
+        public string Gender
+        {
+            get;
+            set;
+        }
+        public string Profile
+        {
+            get;
+            set;
+        }
+        public string CreatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime CreatedDate
+        {
+            get;
+            set;
+        }
+        public string UpdatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime UpdatedDate
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        
+            #region Constructors
+            public Owners()
+            {
+            }
+            public Owners(string OwnerID,string BoutiqueID)
+            {
+                this.OwnerID = OwnerID;
+                this.BoutiqueID = BoutiqueID;
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlDataAdapter sda = null;
+                DataTable dt = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[SelectOwnerByOwnerID]";
+                    cmd.Parameters.Add("@OwnerID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.OwnerID);
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);                    
+                    sda.SelectCommand = cmd;
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    if (dt.Rows.Count == 0) { throw new Exception("No such ID"); }
+                    DataRow row = dt.NewRow();
+                    row = dt.Rows[0];
+                    UserID = row["UserID"].ToString();
+                    Name = row["Name"].ToString();
+                    Address = row["Address"].ToString();
+                    Phone = row["Phone"].ToString();
+                    Email = row["Email"].ToString();
+                    DOB = DateTime.Parse(row["DOB"].ToString());
+                    Gender = row["Name"].ToString();
+                    Profile = row["Profile"].ToString();
+                    CreatedBy = row["CreatedBy"].ToString();
+                    CreatedDate = DateTime.Parse(row["CreatedDate"].ToString());
+                    UpdatedBy = row["UpdatedBy"].ToString();
+                    UpdatedDate = DateTime.Parse(row["UpdatedDate"].ToString());
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+            }
+            #endregion Constructors
+
+            #region New Owner
+        /// <summary>
+        /// to insert a new Owner into database
+        /// </summary>
+        /// <returns>status</returns>
+            public Int16 InsertOwner()
+            {
+                if (BoutiqueID == "")
+                {
+                    throw new Exception("BoutiqueID is Empty!!");
+                }
+                if (UserID == "")
+                {
+                    throw new Exception("UserID is Empty!!");
+                }
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlParameter outParameter = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[InsertOwner]";
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(BoutiqueID);
+                    cmd.Parameters.Add("@UserID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(UserID);
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 255).Value = Name;
+                    cmd.Parameters.Add("@Address", SqlDbType.NVarChar, -1).Value = Address;
+                    cmd.Parameters.Add("@Phone", SqlDbType.NVarChar, 50).Value = Phone;
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar, 100).Value = Email;
+                    cmd.Parameters.Add("@DOB", SqlDbType.DateTime).Value = DOB;
+                    cmd.Parameters.Add("@Gender", SqlDbType.NVarChar, 10).Value = Gender;
+                    cmd.Parameters.Add("@Profile", SqlDbType.NVarChar, -1).Value = Profile;
+                    cmd.Parameters.Add("@CreatedBy", SqlDbType.NVarChar, 255).Value = CreatedBy;
+                    cmd.Parameters.Add("@CreatedDate", SqlDbType.DateTime).Value = DateTime.Now;
+
+                    outParameter = cmd.Parameters.Add("@InsertStatus", SqlDbType.SmallInt);
+                    outParameter.Direction = ParameterDirection.Output;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+                //insert success or failure
+                return Int16.Parse(outParameter.Value.ToString());
+
+            }
+            #endregion 
+
+            #region Edit Owner
+        /// <summary>
+        /// to edit the owner details
+        /// </summary>
+        /// <returns>status</returns>
+            public Int16 UpdateOwner()
+            {
+                if (OwnerID == "")
+                {
+                    throw new Exception("OwnerID is Empty!!");
+                }
+                if (BoutiqueID == "")
+                {
+                    throw new Exception("BoutiqueID is Empty!!");
+                }
+                if (UserID == "")
+                {
+                    throw new Exception("UserID is Empty!!");
+                }
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlParameter outParameter = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[UpdateOwner]";
+                    cmd.Parameters.Add("@OwnerID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(OwnerID);
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(BoutiqueID);
+                    cmd.Parameters.Add("@UserID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(UserID);
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 255).Value = Name;
+                    cmd.Parameters.Add("@Address", SqlDbType.NVarChar, -1).Value = Address;
+                    cmd.Parameters.Add("@Phone", SqlDbType.NVarChar, 50).Value = Phone;
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar, 100).Value = Email;
+                    cmd.Parameters.Add("@DOB", SqlDbType.DateTime).Value = DOB;
+                    cmd.Parameters.Add("@Gender", SqlDbType.NVarChar, 10).Value = Gender;
+                    cmd.Parameters.Add("@Profile", SqlDbType.NVarChar, -1).Value = Profile;
+                    cmd.Parameters.Add("@UpdatedBy", SqlDbType.NVarChar, 255).Value = UpdatedBy;
+                    cmd.Parameters.Add("@UpdatedDate", SqlDbType.DateTime).Value = DateTime.Now;
+
+                    outParameter = cmd.Parameters.Add("@UpdateStatus", SqlDbType.SmallInt);
+                    outParameter.Direction = ParameterDirection.Output;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+                //update success or failure
+                return Int16.Parse(outParameter.Value.ToString());
+
+            }
+            #endregion
+
+            #region Delete an Owner
+        /// <summary>
+        /// To delete an owner by owner id
+        /// </summary>
+        /// <returns>status</returns>
+            public Int16 DeleteOwner()
+            {
+                if (OwnerID == "")
+                {
+                    throw new Exception("OwnerID is Empty!!");
+                }
+                if (BoutiqueID == "")
+                {
+                    throw new Exception("BoutiqueID is Empty!!");
+                }
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlDataAdapter sda = null;
+                DataTable dt = null;
+                SqlParameter outParameter = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    sda = new SqlDataAdapter();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[DeleteOwner]";
+                    cmd.Parameters.Add("@OwnerID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.OwnerID);
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);
+                    outParameter = cmd.Parameters.Add("@DeleteStatus", SqlDbType.SmallInt);
+                    outParameter.Direction = ParameterDirection.Output;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+                //delete success or failure
+                return Int16.Parse(outParameter.Value.ToString());
+            }
+            #endregion
+
+        #region AllOWners
+        /// <summary>
+        /// To get all the owners under a boutique
+        /// </summary>
+        /// <returns></returns>
+            public DataTable GetAllOwners()
+            {
+                if (BoutiqueID == "")
+                {
+                    throw new Exception("BoutiqueID is Empty!!");
+                }
+                dbConnection dcon = null;
+                SqlCommand cmd = null;
+                SqlDataAdapter sda = null;
+                DataTable dt = null;
+                try
+                {
+                    dcon = new dbConnection();
+                    dcon.GetDBConnection();
+                    cmd = new SqlCommand();
+                    sda = new SqlDataAdapter();
+                    cmd.Connection = dcon.SQLCon;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "[SelectAllOwners]";
+                    cmd.Parameters.Add("@BoutiqueID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.BoutiqueID);
+                    sda.SelectCommand = cmd;
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    if (dt.Rows.Count == 0) { throw new Exception("No such item"); }
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (dcon.SQLCon != null)
+                    {
+                        dcon.DisconectDB();
+                    }
+                }
+
+            }
+
+        #endregion
+
+        #endregion Methods
+
     }
 }
