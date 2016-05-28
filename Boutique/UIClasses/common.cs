0df7fda03f3c61959b6a0d73ac71a96bc08e6c45
tvm-gibin <gibin.jacob@thrithvam.me>
2016-05-28 10:44:01 +0530

    Login Msg Style Added,UI Page url,names and roles added

--- Boutique/UIClasses/common.cs
+++ Boutique/UIClasses/common.cs
@@ -49,5 +49,187 @@ namespace Boutique.UIClasses
             }
         }
 
+        #region PageUrl
+
+        public string SaDashBoardPage
+        {
+            get
+            {
+                return "SaDashBoard.aspx";
+            }
+        }
+        public string CategoryPage
+        {
+            get
+            {
+                return "Category.aspx";
+            }
+        }
+        public string DashBoardPage
+        {
+            get
+            {
+                return "DashBoard.aspx";
+            }
+        }
+        public string LoyaltyPage
+        {
+            get
+            {
+                return "Loyalty.aspx";
+            }
+        }
+        public string LoyaltySettingsPage
+        {
+            get
+            {
+                return "LoyaltySettings.aspx";
+            }
+        }
+        public string NotificationsPage
+        {
+            get
+            {
+                return "Notifications.aspx";
+            }
+        }
+        public string PeoplePage 
+        {
+            get
+            {
+                return "People.aspx";
+            }
+        }
+        public string ProductFileUploadPage
+        {
+            get
+            {
+                return "ProductFileUpload.aspx";
+            }
+        }
+        public string ProductsPage
+        {
+            get
+            {
+                return "Products.aspx";
+            }
+        }
+        public string ProfilePage
+        {
+            get
+            {
+                return "Profile.aspx";
+            }
+        }
+        
+        #endregion #region PageUrl
+
+
+        #region PagesName
+
+        public string SaDashBoard
+        {
+            get
+            {
+                return "SaDashBoard";
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return "Category";
+            }
+        }
+        public string DashBoard
+        {
+            get
+            {
+                return "DashBoard";
+            }
+        }
+        public string Loyalty
+        {
+            get
+            {
+                return "Loyalty";
+            }
+        }
+        public string LoyaltySettings
+        {
+            get
+            {
+                return "LoyaltySettings";
+            }
+        }
+        public string Notifications
+        {
+            get
+            {
+                return "Notifications";
+            }
+        }
+        public string People
+        {
+            get
+            {
+                return "People";
+            }
+        }
+        public string ProductFileUpload
+        {
+            get
+            {
+                return "ProductFileUpload";
+            }
+        }
+        public string Products
+        {
+            get
+            {
+                return "Products";
+            }
+        }
+        public string Profile
+        {
+            get
+            {
+                return "Profile";
+            }
+        }
+
+        #endregion PagesName
+
+
+        #region Roles
+
+        public string Manager
+        {
+            get
+            {
+                return "Manager";
+            }
+        }
+
+        public string SuperAdministrator
+        {
+            get
+            {
+                return "SAdmin";
+            }
+        }
+
+        public string Administrator
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        #endregion Roles
+
+
+
     }
 }
