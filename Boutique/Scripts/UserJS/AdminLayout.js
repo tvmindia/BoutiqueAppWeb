4dbd04daffa3bb7f5179edbdd9d1e564e971ae2d
tvm-anija <anija.g@thrithvam.me>
2016-06-08 09:18:18 +0530

    undo changes

---
+++ Boutique/Scripts/UserJS/AdminLayout.js
@@ -0,0 +1,20 @@
+﻿$("document").ready(function (e) {
+
+
+
+
+    $(".navTitle").live({
+        click: function (e) {// Clear controls
+            var navTitle = $(this).closest('li').find('span').text(); 3
+
+            document.title = navTitle;
+            //  ClearCategoryControls();
+            return true;
+        }
+    })
+
+
+
+
+
+});//end of document.ready
