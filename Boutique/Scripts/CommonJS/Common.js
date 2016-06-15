1b51c80cbcd2fb84b6a34992c0a352a56a3fa543
tvm-anija <anija.g@thrithvam.me>
2016-06-15 14:46:05 +0530

    Added custom alert box

--- Boutique/Scripts/CommonJS/Common.js
+++ Boutique/Scripts/CommonJS/Common.js
@@ -116,6 +116,43 @@ function removeCustomAlert() {
     document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
 }
 
+
+function CustomAlert(txt) {
+    d = document;
+
+    if (d.getElementById("modalContainer")) return;
+
+    mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
+    mObj.id = "modalContainer";
+    mObj.style.height = d.documentElement.scrollHeight + "px";
+
+    alertObj = mObj.appendChild(d.createElement("div"));
+    alertObj.id = "alertBox";
+    if (d.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+    alertObj.style.left = (d.documentElement.scrollWidth - alertObj.offsetWidth) / 2 + "px";
+    alertObj.style.visiblity = "visible";
+
+    h1 = alertObj.appendChild(d.createElement("h1"));
+    h1.appendChild(d.createTextNode("Delete"));
+
+    msg = alertObj.appendChild(d.createElement("p"));
+    //msg.appendChild(d.createTextNode(txt));
+    msg.innerHTML = txt;
+
+    btn = alertObj.appendChild(d.createElement("a"));
+    btn.id = "closeBtn";
+    btn.className = "btnButton";
+    btn.appendChild(d.createTextNode("OK"));
+    btn.href = "#";
+    btn.focus();
+    btn.onclick = function () { removeCustomAlert(); return false; }
+
+    alertObj.style.display = "block";
+
+}
+
+
+
 function getJsonData(data, page) {
     var jsonResult = {};
     // $("#loadingimage").show();
