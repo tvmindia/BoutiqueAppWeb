beafb5bb27944c00ba079a85557f0381bb6b9164
tvm-gibin <gibin.jacob@thrithvam.me>
2016-06-16 15:51:48 +0530

    Remove common functions

--- Boutique/Scripts/UserJS/Category.js
+++ Boutique/Scripts/UserJS/Category.js
@@ -146,27 +146,7 @@ function DeleteItem(e,p)
     $("#hdfCategoryID").val('');
 }
 
-//---getting data as json-----//
-function getJsonData(data, page) {
-    var jsonResult = {};
-    // $("#loadingimage").show();
-    var req = $.ajax({
-        type: "post",
-        url: page,
-        data: data,
-        delay: 3,
-        async: false,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json"
 
-    }).done(function (data) {
-
-        //     $("#loadingimage").hide();
-        jsonResult = data;
-    });
-    return jsonResult;
-}
-//---end of getting data as json -----//
 
 
 function BindAsyncCategoryTable()
