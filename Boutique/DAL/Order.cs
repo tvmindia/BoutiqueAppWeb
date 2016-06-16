a75f1fc0139ef7cd185d7afdb3466d0fb21a9698
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-06-16 10:45:26 +0530

    no items flag

--- Boutique/DAL/Order.cs
+++ Boutique/DAL/Order.cs
@@ -265,7 +265,6 @@ namespace Boutique.DAL
                 sda.SelectCommand = cmd;
                 dt = new DataTable();
                 sda.Fill(dt);
-                if (dt.Rows.Count == 0) { throw new Exception("No such item"); }
                 return dt;
             }
             catch (Exception ex)
