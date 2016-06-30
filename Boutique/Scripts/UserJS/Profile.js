13b899cd5ba361690b423c651143529e3fb54540
tvm-shamila <shamila.tp@thrithvam.me>
2016-06-30 17:24:14 +0530

    Bind after order change

--- Boutique/Scripts/UserJS/Profile.js
+++ Boutique/Scripts/UserJS/Profile.js
@@ -286,6 +286,27 @@ $("document").ready(function (e) {
 
             result = UpdateorderNoOfBannerImage(Boutique);
 
+            if (result.status == "1") {
+            
+                BindAllBannerImages();
+            //$('#rowfluidDiv').show();
+            //$('.alert-success').show();
+            //$('.alert-success strong').text(Messages.InsertionSuccessFull);
+
+            //AutoScrollToAlertBox();
+
+
+        }
+               //else {
+               //     $('#rowfluidDiv').show();
+               //     $('.alert-error').show();
+               //     $('.alert-error strong').text(Messages.InsertionFailure);
+               // }
+
+
+           
+
+
         }//when div image is reordered
     });
     $("#BannerImageholder").disableSelection();
@@ -737,7 +758,7 @@ function BindAllBannerImages() {
            + ''
            + '<div class="image-box"><img id="' + totalimages[i].ImageID + '" class="productimage" src="../ImageHandler/ImageServiceHandler.ashx?bannerImgID=' + totalimages[i].ImageID + '"></img></div>'
            + '<div class="productDetailsdiv text-desc">'
-           + '<a class="btn btn-toolbar" style="border:1px solid white" " productno=' + 'A' + '  productid=' + 'A' + ' bannerImgID=' + totalimages[i].ImageID + ' pname=' + 'A' + ' pprice=' + 'A' + ' isoutstock=' + 'A' + ' isactive=' + 'A' + ' categories=' + 'A' + ' designers=' + 'A' + ' designerName=' + 'A' + ' discount=' + 'A' + '><i class="halflings-icon white edit"></i></a>'
+           + '<a class="btn btn-toolbar" style="border:1px solid white" " ProductID=' + totalimages[i].ProductID + '  CategoryCode=' + totalimages[i].CategoryCode + ' bannerImgID=' + totalimages[i].ImageID + ' pname=' + 'A' + ' pprice=' + 'A' + ' isoutstock=' + 'A' + ' isactive=' + 'A' + ' categories=' + 'A' + ' designers=' + 'A' + ' designerName=' + 'A' + ' discount=' + 'A' + '><i class="halflings-icon white edit"></i></a>'
            + '<a class="btn btn-toolbar" style="border:1px solid white" href="../ImageHandler/ImageServiceHandler.ashx?bannerImgID=' + totalimages[i].ImageID + '" data-lightbox="' + totalimages[i].ImageID + '" data-title="' + 'A' + '"><i class="icon-zoom-in"></i></a>'
            //+ '<div class="prodet"><span>Code:  </span><span>' + 'A' + '</span></div><div class="prodet"><span>Name:  </span><span class="proname">' + 'A' + '</span></div><div class="prodet"><span>Price:  ₹  ' + 'A' + '</span></div><div class="prodet><span>Discount:  ₹ ' + 'A' + '</span></span></div><span class="pdescription" style="display:none;">' + 'A' + '</span></div>'
            + '</div>');
