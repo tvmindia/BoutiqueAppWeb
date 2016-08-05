29a13bbc3a2b18b0793bd73386fe0b3599295e41
tvm-anija <anija.g@thrithvam.me>
2016-08-05 11:19:04 +0530

    Added BugTracker properties in MailSending.cs

--- Boutique/DAL/MailSending.cs
+++ Boutique/DAL/MailSending.cs
@@ -104,6 +104,26 @@ namespace Boutique.DAL
             get;
             set;
         }
+        public string status
+        {
+            get;
+            set;
+        }
+        public string BugTrackerVersion
+        {
+            get;
+            set;
+        }
+        public string BugTrackerUserID
+        {
+            get;
+            set;
+        }
+        public string BugTrackerCreatedBy
+        {
+            get;
+            set;
+        }
         #endregion Global Variables
 
         #region Public Variables
