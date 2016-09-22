9fab3c00cccdb5f3ce7688c717131de8141077c8
tvm-gibin <gibin.jacob@thrithvam.me>
2016-09-22 16:01:53 +0530

    datepicker comment removed

--- Boutique/Scripts/custom.js
+++ Boutique/Scripts/custom.js
@@ -317,7 +317,7 @@ function template_functions(){
 	//$('.cleditor').cleditor();
 	
 	/* ---------- Datapicker ---------- */
-	//$('.datepicker').datepicker();
+	$('.datepicker').datepicker();
 	
 	/* ---------- Notifications ---------- */
 	$('.noty').click(function(e){
